32d0239214e080e316ea08d334c99e735c6b92dc
andriihrechko <andriihrechko250703@gmail.com>
2026-03-31 15:35:12 +0300

    Final Solution

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ class CarWashStation:
             * (self.clean_power - car.clean_mark)
             * self.average_rating
         ) / self.distance_from_city_center
-        return price
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
