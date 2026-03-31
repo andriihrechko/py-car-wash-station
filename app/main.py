e1dfbd6a2df6b897c04924326ad5f125962b92f4
andriihrechko <andriihrechko250703@gmail.com>
2026-03-31 15:30:49 +0300

    Final Solution

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class CarWashStation:
                 if car.clean_mark < self.clean_power:
                     full_price += self.calculate_washing_price(car)
                     self.wash_single_car(car)
-        return full_price
+        return round(full_price, 1)
 
     def calculate_washing_price(self, car: Car) -> float | int:
         price = (
@@ -34,7 +34,7 @@ class CarWashStation:
             * (self.clean_power - car.clean_mark)
             * self.average_rating
         ) / self.distance_from_city_center
-        return int(price * 10 + 0.5) / 10
+        return price
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
